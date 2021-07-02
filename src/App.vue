3c969732b532b64ce926153d929f0d9f5b9c6c7b
Gia <gia@coa.vn>
2021-07-02 17:04:29 +0700

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+#app
+  .g-page
+    h1 Gia-PUI - Kitchen sink
+
+    section
+      h2 Buttons
+      p gia-pui includes several predefined button styles, each serving its own semantic purpose, with a few extras thrown in for more control.
+      .types
+        h3 Types
+        g-button(
+          v-for="i in types"
+          :type="i"
+          :key="i") type={{i}}
+        g-button(disabled) disabled
+
+      .sizes
+        h3 Sizes
+        g-button(
+          v-for="i in sizes"
+          :size="i"
+          :key="i") size={{i}}
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Button from '@/components/Button'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    GButton: Button
+  },
+
+  data () {
+    return {
+      types: ['normal', 'primary', 'success', 'warning', 'error'],
+      sizes: ['xs', 'sm', 'md', 'lg', 'xl']
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" scoped>
+.buttons
+  // > *:not(:last-child)
+  //   margin-right 1em
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+button.g-button.g-boxed(:class="classes")
+  slot
+</template>
+
+<script>
+export default {
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+
+    type: {
+      type: String,
+      default: 'default'
+    },
+
+    size: {
+      type: String,
+      default: 'md'
+    }
+  },
+
+  computed: {
+    classes () {
+      return {
+        'g-disabled': this.disabled,
+        [`g-${this.type}`]: true,
+        [`g-${this.size}`]: true
+      }
+    }
+  }
+}
+</script>
